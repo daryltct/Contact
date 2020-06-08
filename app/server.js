4df46359a2710d2f3e491fd975b3adc8272dbf24
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-09 00:46:53 +0800

    Add template for route handling

--- app/server.js
+++ app/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
 const app = express();
+
+//Require Routes
+const authRoutes = require('./routes/auth'),
+	contactsRoutes = require('./routes/contacts'),
+	usersRoutes = require('./routes/users');
+
+//Use Routes: automatically appends url chain infront of respective routes
+app.use('/api/auth', authRoutes);
+app.use('/api/contacts', contactsRoutes);
+app.use('/api/users', usersRoutes);
 
 app.get('/', (req, res) => {
 	res.send('Server initalized');
