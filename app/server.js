3ddafad978772dd3ddcd4b73cc8f0ebb46660f26
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-09 18:22:10 +0800

    Initialize project files for front-end and added React dependencies

--- app/server.js
+++ app/server.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
 	res.send('Server initalized');
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log('Server is listening...');
 });
