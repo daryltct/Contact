a7a81258288fcd2aae1e7101042679aaf332ee86
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-12 01:23:32 +0800

    Navbar dynamically display links

--- app/client/src/components/layout/Navbar.js
+++ app/client/src/components/layout/Navbar.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useContext, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/auth/AuthContext';
 
 function Navbar() {
+	const { isLoggedIn, user, logout } = useContext(AuthContext);
+
+	const authLinks = (
+		<Fragment>
+			<li>Hello, {user && user.name}</li>
+			<li>
+				<a href="#" />
+				<i className="fas fa-sign-out-alt" /> <span className="hide-sm">Logout</span>
+			</li>
+		</Fragment>
+	);
+
+	const guestLinks = (
+		<Fragment>
+			<li>
+				<Link to="/register">Register</Link>
+			</li>
+			<li>
+				<Link to="/login">Login</Link>
+			</li>
+		</Fragment>
+	);
+
 	return (
 		<div className="navbar bg-primary">
 			<Link to="/">
@@ -9,20 +33,7 @@ function Navbar() {
 					<i className="fas fa-id-card-alt" /> Contacts Manager
 				</h1>
 			</Link>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/register">Register</Link>
-				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-			</ul>
+			<ul>{isLoggedIn ? authLinks : guestLinks}</ul>
 		</div>
 	);
 }
