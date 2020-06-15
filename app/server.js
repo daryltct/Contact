675f8ad9e81da779fbd8fa4f7b0b4c3560bf2e31
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-15 20:08:29 +0800

    Prepare for deploy

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-	connectDB = require('./config/db');
+	connectDB = require('./config/db'),
+	path = require('path');
 const app = express();
 
 //Require Routes
@@ -18,9 +19,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/contacts', contactsRoutes);
 app.use('/api/users', usersRoutes);
 
-app.get('/', (req, res) => {
-	res.send('Server initalized');
-});
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Server is listening...');
